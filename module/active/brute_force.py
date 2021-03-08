ca848968592c366e643c433113adc6e124973045
Keppfor <2094289254@qq.com>
2021-03-08 20:39:51 +0800

    新建brute_force模块文件

---
+++ module/active/brute_force.py
@@ -0,0 +1,38 @@
+import sys
+
+
+class BruteForce:
+    """
+    暴力破解
+    """
+
+    def __init__(self, target):
+        """
+        初始化基本信息
+        友情提示：这种注释不用手敲的，等你打好参数，写好return，再到函数第一行敲三个引号回车自动生成
+        :param target: 要扫描的目标域名
+        """
+        self.target = target
+
+    def get_by_baidu(self):
+        """
+        通过百度搜索子域名
+        :return: 搜索到的子域名列表
+        """
+        domain_li = []
+        return domain_li
+
+    def test(self, x, a):
+        """
+
+        :param x:
+        :param a:
+        :return:
+        """
+        a = 1
+        return "xxx"
+
+
+if __name__ == '__main__':
+    search_engine = SearchEngine("xxx.com")
+    search_engine.get_by_baidu()    # 自己测试好功能是否可用
