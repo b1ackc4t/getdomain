5741d2a1f3776e778b38649f8e3e42b2a475e3c1
Keppfor <2094289254@qq.com>
2021-03-29 23:17:20 +0800

    敏感信息收集

--- module/active/SensitiveFile.py
+++ module/active/SensitiveFile.py
@@ -11,6 +11,10 @@ class SentiveFile(object):
     """
 
     def __init__(self, url):
+        """
+        :param url: 外部输入的url
+        :param domians:返回主程序的结果
+        """
         self.url = url
         self.domains = set()
 
@@ -22,21 +26,42 @@ class SentiveFile(object):
     """
 
     def get_sentive_message(self):
+        """
+        files列表可从外部导入文件
+        爬取可能泄露信息页面中的敏感信息
+        :return: self.domains
+        """
         files = [
             "/crossdomain.xml",
             "/robot.txt"
         ]
         res_access = r'<allow-access-from domain="(.*?)"'
-        domain = ''
         try:
             for file in files:
                 r = get(self.url + file)
                 domain = re.findall(res_access, r.text)
+                # ['a', 'a', 'g', 'https://sdfsjf', 'http://sdfjhksdfjhkl', 'k3elwjk']  #
                 if len(domain) != 0:
                     for i in range(len(domain)):
                         self.domains.add(domain[i])
         except exceptions.RequestException as e:
             print(e)
+        print(self.domains)
+
+    def get_clean(self):
+        """
+        初清洗 暂时只褪去http://
+        :return:
+        """
+        unwashed_domains = self.domains
+        cleaned_domains = set()
+        for domain in unwashed_domains:
+            if 'http://' in domain:
+                domain = domain.replace('http://', '')
+            if 'https://' in domain:
+                domain = domain.replace('https://', '')
+            cleaned_domains.add(domain)
+        self.domains = cleaned_domains
         print(self.domains)
 
 
@@ -45,6 +70,8 @@ def main(url):
     sentive_file = SentiveFile(url)
     # 获取页面内容中敏感信息
     sentive_file.get_sentive_message()
+    # 清洗数据
+    sentive_file.get_clean()
 
 
 if __name__ == '__main__':
